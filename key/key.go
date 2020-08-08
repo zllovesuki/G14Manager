389865b25056e362915b16509a37d25be9c6090e
Rachel Chen <me@rachelchen.me>
2020-08-07 19:46:38 -0700

    added key press demo

---
+++ key/key.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/zllovesuki/ROGManager/system/atkacpi"
+)
+
+func main() {
+	ctrlSet, err := atkacpi.NewAtkControl(atkacpi.WriteControlCode)
+	if err != nil {
+		panic(err)
+	}
+
+	inputBuf := make([]byte, atkacpi.KeyPressControlBufferLength)
+	copy(inputBuf, atkacpi.KeyPressControlBuffer)
+	inputBuf[atkacpi.KeyPressControlByteIndex] = byte(0x38) // emulate pressing the ROG Key
+
+	writeOutput, err := ctrlSet.Write(inputBuf)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(writeOutput)
+
+	ctrlGet, err := atkacpi.NewAtkControl(atkacpi.ReadControlCode)
+	if err != nil {
+		panic(err)
+	}
+
+	readOutput, err := ctrlGet.Read(atkacpi.BatteryChargeLimitOutputBufferLength)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(readOutput)
+}

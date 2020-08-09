b7f8d00878ec2f44d5f8fcb5f9e109b4b77ade8d
Rachel Chen <me@rachelchen.me>
2020-08-09 04:55:06 -0700

    Fix build bs

--- .github/build.ps1
+++ .github/build.ps1
@@ -3,15 +3,28 @@
 # should have Mingw-w64 installed already
 Get-Command "gcc.exe"
 
-$CommandInstallRSRC = "go get github.com/akavel/rsrc"
-Invoke-Expression $CommandInstallRSRC
-
-# Fail if we don't have rsrc
-Get-Command "rsrc.exe"
-
+$env:GOPATH = $env:GITHUB_WORKSPACE + "\go"
+$GOBIN = $env:GOPATH + "\bin"
+$env:Path += ";" + $env:GOBIN
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
 $env:GOHOSTARCH = "amd64"
 $env:CGO_ENABLED = "1"
 
-go build -tags "use_cgo" -ldflags="-H=windowsgui -s -w" .
+Write-Host $env:Path
+
+$CommandInstallRSRC = "go get github.com/akavel/rsrc"
+Invoke-Expression $CommandInstallRSRC
+
+# easier for us to debug
+Get-ChildItem $GOBIN
+
+$RSRC = $GOBIN + "\rsrc.exe -arch amd64 -manifest ROGManager.exe.manifest -ico go.ico -o ROGManager.exe.syso"
+Invoke-Expression $RSRC
+
+# Stupid go mod download writes to stderr
+$MOD = "go mod download"
+Invoke-Expression $MOD 2>&1
+
+$BUILD = "go build -tags 'use_cgo' -ldflags='-H=windowsgui -s -w' ."
+Invoke-Expression $BUILD
--- changelog.sh
+++ changelog.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-changes=$(git log $1 --pretty=format:"[view commit](http://github.com/zllovesuki/ROGManager/commit/%H) %s\n")
-changelog="# $2\n$changes"
-echo -e "$changelog\n\n$(cat Changelog.md)" > Changelog.md
+changes=$(git log $1 --pretty=format:"(%h) - %s\n")
+changelog="$2\n $changes"
+echo -e $changelog
+read -p "looks good?" yn
+case $yn in
+    [Yy]* ) git tag -a -m "$changelog" $2;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+esac

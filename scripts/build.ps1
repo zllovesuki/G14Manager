a4699cd9b3b343ae62cf7aca69bef6f7008e8ca7
Rachel Chen <me@rachelchen.me>
2020-08-22 23:43:26 -0700

    additional hid initialization buffer

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,4 +10,4 @@ $env:CDO_ENABLED = 1
 rsrc.exe -arch amd64 -manifest G14Manager.exe.manifest -ico go.ico -o G14Manager.exe.syso
 
 go build -ldflags="-H=windowsgui -s -w" -o "build/G14Manager.exe" .
-go build -o "build/G14Manager.debug.exe" .
+go build -gcflags="-N -l" -o "build/G14Manager.debug.exe" .

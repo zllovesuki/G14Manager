a3038c504b03b249b057737e8883bddd6be95f00
Rachel Chen <rachel@chens.email>
2020-12-09 13:06:37 -0800

    rpc: make changes to protobuf and implement two servers

--- rpc/server/battery.go
+++ rpc/server/battery.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/zllovesuki/G14Manager/rpc/protocol"
+	"github.com/zllovesuki/G14Manager/system/battery"
 
 	empty "github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
@@ -11,18 +14,63 @@ import (
 
 type BatteryServer struct {
 	protocol.UnimplementedBatteryChargeLimitServer
+
+	mu      sync.RWMutex
+	control *battery.ChargeLimit
 }
 
 var _ protocol.BatteryChargeLimitServer = &BatteryServer{}
 
-func New(s *grpc.Server) {
-	protocol.RegisterBatteryChargeLimitServer(s, &BatteryServer{})
+func RegisterBatteryChargeLimitServer(s *grpc.Server, ctrl *battery.ChargeLimit) *BatteryServer {
+	server := &BatteryServer{
+		control: ctrl,
+	}
+	protocol.RegisterBatteryChargeLimitServer(s, server)
+	return server
 }
 
-func (b *BatteryServer) GetCurrent(ctx context.Context, _ *empty.Empty) (*protocol.BatteryChargeLimitResponse, error) {
-	return nil, nil
+func (b *BatteryServer) GetCurrentLimit(ctx context.Context, _ *empty.Empty) (*protocol.BatteryChargeLimitResponse, error) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	if b.control == nil {
+		return nil, fmt.Errorf("battery server is not initialized")
+	}
+
+	resp := &protocol.BatteryChargeLimitResponse{
+		Success:    true,
+		Percentage: uint32(b.control.CurrentLimit()),
+	}
+
+	return resp, nil
 }
 
 func (b *BatteryServer) Set(ctx context.Context, req *protocol.SetBatteryLimitRequest) (*protocol.BatteryChargeLimitResponse, error) {
-	return nil, nil
+	if req == nil {
+		return nil, fmt.Errorf("nil request is invalid")
+	}
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	if b.control == nil {
+		return nil, fmt.Errorf("battery server is not initialized")
+	}
+
+	resp := &protocol.BatteryChargeLimitResponse{}
+	err := b.control.Set(uint8(req.GetPercentage()))
+	if err != nil {
+		resp.Success = false
+		resp.Message = err.Error()
+	} else {
+		resp.Success = true
+		resp.Percentage = uint32(b.control.CurrentLimit())
+	}
+	return resp, nil
+}
+
+func (b *BatteryServer) HotReload(ctrl *battery.ChargeLimit) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.control = ctrl
 }

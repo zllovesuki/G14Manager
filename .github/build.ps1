08272062ec90ba4ed98b0708ccda3911bc575578
Rachel Chen <rachel@chens.email>
2021-04-01 00:11:50 -0700

    builder: use local git tag for variable injection

--- .github/build.ps1
+++ .github/build.ps1
@@ -3,13 +3,14 @@
 # should have Mingw-w64 installed already
 Get-Command "gcc.exe"
 
-$env:GOPATH = $env:GITHUB_WORKSPACE + "\go"
-$GOBIN = $env:GOPATH + "\bin"
+# $env:GOPATH = $env:GITHUB_WORKSPACE + "\go"
+# $GOBIN = $env:GOPATH + "\bin"
 $env:Path += ";" + $env:GOBIN
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
 # $env:GOHOSTARCH = "amd64"
 $env:CGO_ENABLED = "1"
+$LatestTag = (git describe --abbrev=0 --tags)
 
 Write-Host $env:Path
 
@@ -17,7 +18,7 @@ $CommandInstallRSRC = "go get github.com/akavel/rsrc"
 Invoke-Expression $CommandInstallRSRC
 
 # easier for us to debug
-Get-ChildItem $GOBIN
+# Get-ChildItem $GOBIN
 
 $RSRC = $GOBIN + "\rsrc.exe -arch amd64 -manifest .\cmd\manager\G14Manager.exe.manifest -ico .\cmd\manager\go.ico -o .\cmd\manager\G14Manager.exe.syso"
 Invoke-Expression $RSRC
@@ -34,17 +35,17 @@ Invoke-Expression $PACKED
 Write-Host "Building prod release"
 
 # $BUILD = "go build -tags 'use_cgo' -ldflags='-H=windowsgui -s -w' ."
-$BUILD = "go build -ldflags=`"-H=windowsgui -s -w -X 'main.Version=$env:GITHUB_REF' -X 'main.IsDebug=no'`" -o build/G14Manager.exe .\cmd\manager"
+$BUILD = "go build -ldflags=`"-H=windowsgui -s -w -X 'main.Version=$LatestTag' -X 'main.IsDebug=no'`" -o build/G14Manager.exe .\cmd\manager"
 Invoke-Expression $BUILD
 
 Write-Host "Building debug release"
 
-$BUILD_DEBUG = "go build -gcflags=`"-N -l`" -ldflags=`"-X 'main.Version=$env:GITHUB_REF-debug' -X 'main.IsDebug=yes'`" -o build/G14Manager.debug.exe .\cmd\manager"
+$BUILD_DEBUG = "go build -gcflags=`"-N -l`" -ldflags=`"-X 'main.Version=$LatestTag-debug' -X 'main.IsDebug=yes'`" -o build/G14Manager.debug.exe .\cmd\manager"
 Invoke-Expression $BUILD_DEBUG
 
 Write-Host "Building configurator"
 
-$BUILD_CLIENT = "go build -ldflags=`"-X main.Version=$env:GITHUB_REF'`" -o build/G14Manager.config.exe .\cmd\client"
+$BUILD_CLIENT = "go build -ldflags=`"-X main.Version=$LatestTag'`" -o build/G14Manager.config.exe .\cmd\client"
 Invoke-Expression $BUILD_CLIENT
 
 Write-Host "Building DLLs"
